1892554f02e76db263a36e7814ee352657b74aa1
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-03-13 10:03:09 +0100

    add useEffect to send http request when cart change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useEffect } from 'react';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 import {useSelector} from 'react-redux';
 
+
+
 function App() {
 
   const isShow = useSelector(state=> state.ui.isShow)
+
+  const cart= useSelector(state=> state.cart)
+
+  useEffect(()=>{
+    fetch('https://react-http-c7d71-default-rtdb.firebaseio.com/cart.json',{
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    })
+  }, [cart])
 
   return (
     <Layout>
